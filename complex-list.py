2700f452d7d8d81958b0107618877219d7039132
Mark Hofstetter <mark@hofstetter.at>
2018-04-23 15:19:17 +0200

    aufgabe liste

--- complex-list.py
+++ complex-list.py
@@ -1,12 +1,37 @@
-teilnehmer = [
+'''
++ eingabe eines names durch den user
++ dann ausgabe der letzten Ausbildung (so vorhanden)
+
+zb 
+Eingabe: Martin
+Ausgabe: Uni
+
+Eingabe: Ivan
+Ausgabe: n/a
+'''
+
+import pprint
+
+participents = [
     ['Martin',    1976, ['VS', 'Gym', 'Uni']],
     ['Harald',    1970],
-    ['Eva',        1973],
+    ['Eva',       1973],
     ['Roland',    1998],
     ['Christoph', 1981],
     ['Roland',    1974],
+    ['Reza',      1994, ['Informatik']],
+    ['Ivan',      1985],
+    ['Daniel',    1990],
+    ['Reinhard',  1987],
+    ['Alexander', 1986],
+    ['Mark',      1975],       
 ]
 
-teilnehmer.append(['Michala', 1975, ['VS', 'Gym', 'Uni']])
-teilnehmer.sort()
-print(teilnehmer)
+participents.append(['Michala', 1975, ['VS', 'Gym', 'Uni']])
+participents.sort()
+
+pprint.pprint(participents)
+
+
+for participent in participents:
+    print(participent[0])
