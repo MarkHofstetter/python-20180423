bb4bdcb19989b36453c48706c67b5c880b972e0b
Mark Hofstetter <mark@hofstetter.at>
2018-04-23 13:25:28 +0200

    format in one line

--- format.py
+++ format.py
@@ -7,8 +7,7 @@ print("Die Summe von " + str(a1) + " und " + str(a2) + " ist " + str(summe))
 # pythonic
 
 
-print('Die Summe von {} und {} ist {}' \
-                       .format(a1,a2,summe))
+print('Die Summe von {} und {} ist {}'.format(a1,a2,summe))
 
 print('Die Summe von % 5u und %d ist %d' % (a1,a2,summe))
          
