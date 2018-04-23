44457d15dd6ce45fde2b2eeee3317b9e9e2d3425
Mark Hofstetter <mark@hofstetter.at>
2018-04-23 13:15:40 +0200

    removed unecessary var

--- format.py
+++ format.py
@@ -5,10 +5,10 @@ summe = a1 + a2
 print("Die Summe von " + str(a1) + " und " + str(a2) + " ist " + str(summe))
 
 # pythonic
-formatted_result = 'Die Summe von {} und {} ist {}' \
-                       .format(a1,a2,summe)
 
-print(formatted_result)
+
+print('Die Summe von {} und {} ist {}' \
+                       .format(a1,a2,summe))
 
 print('Die Summe von % 5u und %d ist %d' % (a1,a2,summe))
          
