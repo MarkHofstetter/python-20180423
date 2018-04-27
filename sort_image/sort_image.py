789f212f4dbe4846444c086ddab07b83e142b6a4
Mark Hofstetter <mark@hofstetter.at>
2018-04-27 14:21:46 +0200

    sort image

---
+++ sort_image/sort_image.py
@@ -0,0 +1,17 @@
+'''
++ quell und zielverzeichnis auf einer config 
++ verzeichnis rekrusiv durchsuchen
++ a/b/c/diese datei.jfif => a_b_c_diese_datei.jfif
++...
++ testbar
+
+spaces mit underscore ersetzen 
+
++ zusatzaufgabe bilder resizen auf max_length 100
+
+'''
+
+
+
+
+
